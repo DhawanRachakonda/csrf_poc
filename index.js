93437e5680f59ad5182535f9b1ee097976518cd6
H352615 <dhawan.rachakonda@honeywell.com>
2022-11-02 16:59:04 +0530

    feat: POC on csrf

---
+++ index.js
@@ -0,0 +1,79 @@
+const { csrfSync } = require("csrf-sync");
+const express = require("express");
+const session = require("express-session");
+var cors = require('cors');
+const cookieParser = require('cookie-parser');
+const Crypto = require('crypto')
+
+const app = express();
+const port = 5555;
+
+app.set('view engine','ejs')
+
+app.use(
+    session({
+      // Don't do this, use a cryptographically random generated string
+      secret: Crypto
+      .randomBytes(21)
+      .toString('base64')
+      .slice(0, 21)
+    })
+);
+
+app.use(cookieParser());
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+const getTokenFromState = (req) => {
+    console.log("Getting token in state ", req.session.csrfToken);
+  return req.session.csrfToken;
+}
+
+const isRequestValid = (req) => {
+    const receivedToken = req.cookies["X-csrf-token"];
+    const storedToken = getTokenFromState(req);
+    console.log("Received Token : ", receivedToken);
+    return (typeof receivedToken === "string" &&
+        typeof storedToken === "string" &&
+        receivedToken === storedToken);
+};
+
+const csrfSynchronisedProtection = (req, res, next) => {
+    const isCsrfValid = isRequestValid(req);
+    if (!isCsrfValid) {
+        return next(invalidCsrfTokenError);
+    }
+    next();
+}
+
+const { generateToken, revokeToken, invalidCsrfTokenError }  = csrfSync({
+    getTokenFromState, // Used to retrieve the token from state.
+    storeTokenInState : (req, token) => {
+        console.log("Storing token in state ", token);
+      req.session.csrfToken = token;
+    }, // Used to store the token in state.
+    size : 128, // The size of the generated tokens in bits
+    csrfSynchronisedProtection,
+  });
+
+app.get("/csrf-token", (req, res) => {
+    const token = generateToken(req);
+    res.cookie('X-csrf-token',token, { maxAge: 900000, httpOnly: true, secure: true});
+  return res.render('index', {csrfToken: token});
+});
+
+app.get("/hello", (req, res) => {
+  return res.send("Hello World!");
+});
+
+const myProtectedRoute = (req, res) =>
+  res.json({ unpopularOpinion: "Game of Thrones was amazing" });
+
+app.post("/secret-stuff", csrfSynchronisedProtection, myProtectedRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+https://portswigger.net/web-security/csrf/tokens
+
+
+
+https://blog.qualys.com/vulnerabilities-threat-research/2015/01/14/do-your-anti-csrf-tokens-really-protect-your-applications-from-csrf-attack
+
+
+
+I recommend using the below mechanism to implement protection against CSRF using Synchronizer Token Pattern
+
+https://medium.com/@kaviru.mihisara/synchronizer-token-pattern-e6b23f53518e
